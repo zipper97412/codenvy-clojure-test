8d7f31d80723c79a6a69013b49485d618c701394
zipper <zipper97412@gmail.com>
2016-08-26 12:25:37 +0000

    yoloo

--- test-clj/src/test_clj/core.clj
+++ test-clj/src/test_clj/core.clj
@@ -1,7 +1,85 @@
 (ns test-clj.core
   (:gen-class))
 
+
+
+
+(defn- make-row
+  ([nb] (make-row nb 0))
+  ([nb init]
+   (vec (repeat nb init))))
+
+(defn make-grid
+  ([nbrow nbcol] (make-grid nbrow nbcol 0))
+  ([nbrow nbcol init]
+   (vec (repeat nbcol (make-row nbrow init)))))
+
+
+(defn getbyxy
+  ([coll x y]
+   (getbyxy coll x y 0))
+
+  ([coll x y err]
+   (if (and (>= y 0) (>= x 0) (< y (.length coll)) (< x (.length (nth coll y))))
+    (nth (nth coll y) x)
+    err)))
+
+
+(defn assocbyxy
+  [coll x y val]
+  (assoc coll y (assoc (nth coll y) x val)))
+
+(defn aliveCell
+  [grid [x y]]
+  (assocbyxy grid x y 1))
+
+(defn killCell
+  [grid [x y]]
+  (assocbyxy grid x y 0))
+
+(defn initGrid
+  [grid points]
+  (reduce aliveCell grid points))
+
+(defn nextStateCell
+  [grid [x y]]
+  (let [nbAlive
+        (+ (getbyxy grid (dec x) y)
+           (getbyxy grid (inc x) y)
+           (getbyxy grid x (dec y))
+           (getbyxy grid x (inc y))
+           (getbyxy grid (dec x) (dec y))
+           (getbyxy grid (dec x) (inc y))
+           (getbyxy grid (inc x) (dec y))
+           (getbyxy grid (inc x) (inc y)))]
+       (or (= nbAlive 3) (and (= (getbyxy grid x y) 1) (= nbAlive 2)))))
+
+(defn stepCell
+  [previous]
+  (fn [grid point]
+      (if (nextStateCell previous point)
+        (aliveCell grid point)
+        (killCell grid point))))
+
+(defn display-grid
+  [grid]
+  (doall (for [line grid]
+          (println line))))
+
+(defn step-grid
+  [grid]
+  (reduce (stepCell grid) grid (for [y (range 0 (.length grid))
+                                     x (range 0 (.length (nth grid 0)))]
+                                    [x y])))
+
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+ "I don't do a whole lot ... yet."
+ [& args]
+ (loop [gr (-> (make-grid 15 15) (initGrid '([6 6] [6 7] [6 8])))
+        count 10]
+  (do
+    (display-grid gr)
+    (newline)
+    (if (> count 0)
+      (recur (step-grid gr) (dec count))
+      nil))))
